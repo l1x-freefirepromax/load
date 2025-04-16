ffc94689c00832a7b261855b1aa30eb41f1c8cdb
l1x-freefirepromax <lixuy.rn@outlook.com>
2025-04-16 21:10:58 +0700

    Update and rename README.md to .lua

---
+++ .lua
@@ -0,0 +1,36 @@
+task.spawn(function()
+	local decalsyeeted = true
+	local Terrain = workspace.Terrain
+	Terrain.WaterWaveSize = 0
+	Terrain.WaterWaveSpeed = 0
+	Terrain.WaterReflectance = 0
+	Terrain.WaterTransparency = 0
+	game.Lighting.GlobalShadows = false
+	game.Lighting.FogEnd = 9e9
+	game.Lighting.Brightness = 0
+	settings().Rendering.QualityLevel = "Level01"
+	for i, v in pairs(game:GetDescendants()) do
+		if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+			v.Material = "Plastic"
+			v.Reflectance = 0
+		elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+			v.Transparency = 1
+		elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+			v.Lifetime = NumberRange.new(0)
+		elseif v:IsA("Explosion") then
+			v.BlastPressure = 1
+			v.BlastRadius = 1
+		elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") then
+			v.Enabled = false
+		elseif v:IsA("MeshPart") then
+			v.Material = "Plastic"
+			v.Reflectance = 0
+			v.TextureID = "10385902758728957"
+		end
+	end
+	for i, e in pairs(game.Lighting:GetChildren()) do
+		if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+			e.Enabled = false
+		end
+	end
+end)
